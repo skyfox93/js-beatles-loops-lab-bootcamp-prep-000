464e16fc1581eede1db6f6150469ecf59e1e916e
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-26 03:09:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // add solution here
+<<<<<<< HEAD
 function theBeatlesPlay(artists,instruments){var bandList=[];for(var i=0;i<artists.length;i++){ bandList.push(`${artists[i]} plays ${instruments[i]}`);}return bandList;}
 function johnLennonFacts(facts){var i=0;while(i<facts.length){facts[i]=String(facts[i])+'!!!';i++}return facts}
 function iLoveTheBeatles(n){var talk=[];do{talk.push("I love the Beatles!");n+=1;} while(n<15);return talk;}
+=======
+function theBeatlesPlay(artists,instruments){var bandList=[];for(var i=0;i<artists.length;i++){ bandlist.push(`${artist[i]} plays ${instruments[i]}`);}}
+function johnLennonFacts(facts){var i=0; while(i<facts.length){facts[i]+='!!!';i++}}
+function iLoveTheBeatles(n){var talk=[];do{talk.push("I love the Beatles!");n+=1;} while(n<15);}
+>>>>>>> 9514d9db2429732c4183108559d91259386f2551
